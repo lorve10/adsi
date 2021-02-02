75c81608cd37aa4c15573e625c7adbad68264ed1
lorve10 <75343322+lorve10@users.noreply.github.com>
2021-02-01 20:59:30 -0500

    sesión principal
    se envió sesión principal

---
+++ vista/principal.sesion.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once ("header.php") ?>
+<body>
+
+  
+    <div class="container col-9">
+    
+        <h1 id="ofertas"></h1>
+        
+
+    </div>
+
+    <div style="height: 23px;"></div>
+
+  
+
+    <br>
+    <?php include_once("footer.php"); ?>
+   
+</body>
+
+<script src="../proyecto.seres/js/ofertas.js"></script>
+</html>
