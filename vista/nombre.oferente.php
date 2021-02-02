e0f6c7c660b4213a0253334a11545aabd981fe6a
lorve10 <75343322+lorve10@users.noreply.github.com>
2021-02-01 20:57:23 -0500

    nombreOferente
    agrego ventana nombre oferente

---
+++ vista/nombre.oferente.php
@@ -0,0 +1,113 @@
+<?php include_once ("header.php") ?>
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <div class="container border justify-content-center">
+        <table class="containr border-bottom">
+        <tr>
+            <td class="container-fluid" style="width: 60%; height: 60%;" >
+                 <img src="../proyecto.seres/img/blan.jpg"   style="width: 100px; height: 100px; float: left;" class="rounded-circle border" alt="">
+                 <h3>Nombre Oferente</h3>
+                    <p>Ciudad, Departamento</p>
+                        <li>Soy desarrollador de aplicaciones multiplataforma, manejo los lenguajes php</li>
+                        <li>java..************************************************************</li>
+                        
+                        
+                             <i class="fas fa-phone text-end "style="float:left">
+                                <br> Llamar</i>
+                             <i class="far fa-envelope  text-center">
+                                <br> Email</i>
+                             <i class="fab fa-whatsapp  text-center">
+                                 <br>whatsapp</i>
+
+            </td>
+            <td style="width: 30%; height: 30%;" >
+                <a href="principal.sesion.php" class="btn btn-primary text-center">Atras</a>
+                <br>
+                <strong> Calificacion </strong>
+                <br>
+                <div class="ec-stars-wrapper " style="color: yellow;">
+                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                </div>
+
+            </td>
+        </tr>
+        <tr>
+            <td class="border-top justify-content-start">
+                 <h2>Programacion(Categoria)</h2>
+                 <br>
+                 <h4>desarrollo de Apps Android</h4>
+                 <p class="justify-content-start text-ritgh">
+                     ¿Necesitas una app para tu negocio o empresa? Te puedo</br> 
+                     ayudar,puedo desarrollar la aplicaciòn que necesitas  </br>
+                     para tu negocio *************************************</br>
+                     *****************************************************</br>
+                     </p>
+            </td>
+            <td cellpadding="10">
+                <strong class="justify-content-end">$0</strong>
+                <p class="border-top"> publicado hace 4 horas</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="border-top">
+                <h2>Comentarios sobre la oferta</h2>
+                <br>
+                <h4>Nombre del Usuario</h4>
+                <p> ***************************************************</br>
+                    ***************************************************</br>
+                    ***************************************************</br>
+                </p>
+            </td>
+            <td style="width: 30%; height: 30%;" >
+                <strong> Calificacion </strong>
+                <br>
+                <div class="ec-stars-wrapper ">
+                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                </div>
+
+            </td>
+        </tr> 
+        <tr>
+            <td class="border-top">
+                <h3>Nombre del Usuario</h3>
+                <p> ***************************************************</br>
+                    ***************************************************</br>
+                    ***************************************************</br>
+                </p>
+            </td>
+            <td style="width: 30%; height: 30%;" >
+                <strong> Calificacion </strong>
+                <br>
+                <div class="ec-stars-wrapper ">
+                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                </div>
+
+            </td>
+        </tr>
+
+        </table>
+    </div>
+
+    <br>
+    <div class="text-center">
+        <a href="principal.sesion.php" class="btn btn-success">Calificar Oferta</a>
+        </div>
+        <br>
+
+<?php include_once("footer.php"); ?>
+</body>
+</html>
